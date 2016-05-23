fad32216777efa501a0214e3e28eb5753e729401
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-05-22 23:25:26 -0700

    add tmp folder with test image for testing

--- basic-http-server/requestHandlers.js
+++ basic-http-server/requestHandlers.js
@@ -33,7 +33,7 @@ function upload(response, postData) {
 function show(response) {
   console.log("Request handler 'show' was called.");
   response.writeHead(200, {'Content-Type': 'image/png'});
-  fs.createReadStream('/tmp/test.png').pipe(response);
+  fs.createReadStream(__dirname + '/tmp/test.png').pipe(response);
 }
 
 exports.start = start;
