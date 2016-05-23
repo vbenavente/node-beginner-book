89ae3acd50b4a1bd78deb15cd7d268c6d2cb5552
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-05-23 00:03:56 -0700

    added require formdible and renaming upload code

--- basic-http-server/requestHandlers.js
+++ basic-http-server/requestHandlers.js
@@ -1,8 +1,8 @@
 var querystring = require('querystring'),
-    fs = require('fs');
+    fs = require('fs'),
+    fromidable = require('formidable');
 
-
-function start(response, postData) {
+function start(response) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>' +
@@ -13,7 +13,7 @@ function start(response, postData) {
     '<body>' +
     '<form action="/upload" enctype="multipart/form-data" ' +
     'method="post">' +
-    '<input type="submit" value="Upload file" />' +
+    '<input type="file" value="Upload file" />' +
     '</form>' +
     '</body>' +
     '</html>';
@@ -23,11 +23,25 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('You\'ve sent the text: ' + querystring.parse(postData).text);
+
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, (__dirname + '/tmp/test.png'), function(error) {
+      if (error) {
+        fs.unlink(__dirname + '/tmp/test.png');
+        fs.rename(files.upload.path, __dirname + '/tmp/test.png');
+      }
+  });
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('received image:<br/>');
+  response.write("<img src='/show' />");
   response.end();
+  });
 }
 
 function show(response) {
